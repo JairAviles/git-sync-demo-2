351ffc82972bdcb5aeb316d854f5ace81b8e31c6
Segment Git Sync <friends@segment.com>
2025-02-22 03:56:33 +0000

    User Group '2tNdiNIkwzalFl0r45TOsgttrIH' created by jair.aviles@segment.com

---
+++ user_groups/2tNdiNIkwzalFl0r45TOsgttrIH.tf
@@ -0,0 +1,77 @@
+import {
+  to = segment_user_group.id-2tNdiNIkwzalFl0r45TOsgttrIH
+  id = "2tNdiNIkwzalFl0r45TOsgttrIH"
+}
+
+resource "segment_user_group" "id-2tNdiNIkwzalFl0r45TOsgttrIH" {
+  members = []
+  name    = "extensions-demo-1 - Personas Admin"
+  permissions = [
+    {
+      resources = [
+        {
+          id = "9uXW1jxiwcBVdYmovz4gki"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1WDUuSVopUPUzSLNKqvG6uCiFtq"
+    },
+    {
+      resources = [
+        {
+          id = "9uXW1jxiwcBVdYmovz4gki"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1WDUuSmhpuFwR0dtYcgzeiGnmDr"
+    },
+    {
+      resources = [
+        {
+          id = "9uXW1jxiwcBVdYmovz4gki"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1WDUuWtxCUipR4uma0pqqZ8T2Qq"
+    },
+    {
+      resources = [
+        {
+          id = "9uXW1jxiwcBVdYmovz4gki"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1WDUuYzBBvlpH0Oq4hPVMw7OsiW"
+    },
+    {
+      resources = [
+        {
+          id = "9uXW1jxiwcBVdYmovz4gki"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1Y86RP8BjOpdJtb5FbH0Z2M57PX"
+    },
+    {
+      resources = [
+        {
+          id = "9uXW1jxiwcBVdYmovz4gki"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1ZxBLEwfZgYG3olVPxkc7e35Wue"
+    },
+  ]
+}
