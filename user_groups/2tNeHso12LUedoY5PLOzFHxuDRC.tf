436017c752d49158be08affbbe4c3a177198e85a
Segment Git Sync <friends@segment.com>
2025-02-22 04:01:15 +0000

    User Group '2tNeHso12LUedoY5PLOzFHxuDRC' created by jair.aviles@segment.com

---
+++ user_groups/2tNeHso12LUedoY5PLOzFHxuDRC.tf
@@ -0,0 +1,22 @@
+import {
+  to = segment_user_group.id-2tNeHso12LUedoY5PLOzFHxuDRC
+  id = "2tNeHso12LUedoY5PLOzFHxuDRC"
+}
+
+resource "segment_user_group" "id-2tNeHso12LUedoY5PLOzFHxuDRC" {
+  members = []
+  name    = "extensions-demo-1 - Success Engineers"
+  permissions = [
+    {
+      resources = [
+        {
+          id = "9uXW1jxiwcBVdYmovz4gki"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1WDUuSF1kN1xis2lp09RVKOsnnL"
+    },
+  ]
+}
