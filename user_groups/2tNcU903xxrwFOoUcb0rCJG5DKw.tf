9721aeb2d3d2973dac40c95e5ad086d40e2fc7a8
Segment Git Sync <friends@segment.com>
2025-02-22 03:46:26 +0000

    User Group '2tNcU903xxrwFOoUcb0rCJG5DKw' created by jair.aviles@segment.com

---
+++ user_groups/2tNcU903xxrwFOoUcb0rCJG5DKw.tf
@@ -0,0 +1,33 @@
+import {
+  to = segment_user_group.id-2tNcU903xxrwFOoUcb0rCJG5DKw
+  id = "2tNcU903xxrwFOoUcb0rCJG5DKw"
+}
+
+resource "segment_user_group" "id-2tNcU903xxrwFOoUcb0rCJG5DKw" {
+  members = []
+  name    = "extensions-demo-1 - Data Science Personas"
+  permissions = [
+    {
+      resources = [
+        {
+          id = "4gXPjedRH3hxmudTY8DVZw"
+          labels = [
+          ]
+          type = "SOURCE"
+        },
+      ]
+      role_id = "1WDUuNOXQYGLtEI7LcfBMpvU9ll"
+    },
+    {
+      resources = [
+        {
+          id = "9uXW1jxiwcBVdYmovz4gki"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1WDUuY68jLTuIRC6DzqM6zKnkSf"
+    },
+  ]
+}
