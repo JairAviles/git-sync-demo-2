6942aa61f7f57c350898c499cd58517248889d28
Jair Aviles <jair.aviles@segment.com>
2025-02-24 11:47:24 -0800

    uncomment import block in new group

--- user_groups/2ezkdM1wYRnKEbOnsZdc14Xyk1s.tf
+++ user_groups/2ezkdM1wYRnKEbOnsZdc14Xyk1s.tf
@@ -1,7 +1,7 @@
-# import {
-#   to = segment_user_group.id-2tNddhShqTcyTW8bchjXjPMegsJ
-#   id = "2ezkdM1wYRnKEbOnsZdc14Xyk1s"
-# }
+import {
+  to = segment_user_group.id-2tNddhShqTcyTW8bchjXjPMegsJ
+  id = "2ezkdM1wYRnKEbOnsZdc14Xyk1s"
+}
 
 resource "segment_user_group" "id-2ezkdM1wYRnKEbOnsZdc14Xyk1s" {
   members = []
