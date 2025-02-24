de9ef2e4d7ed6fcfb282229e29ac03d8081a937c
Jair Aviles <jair.aviles@segment.com>
2025-02-24 12:02:46 -0800

    add permissions

--- user_groups/2ezkdM1wYRnKEbOnsZdc14Xyk1s.tf
+++ user_groups/2ezkdM1wYRnKEbOnsZdc14Xyk1s.tf
@@ -5,6 +5,18 @@ import {
 
 resource "segment_user_group" "id-2ezkdM1wYRnKEbOnsZdc14Xyk1s" {
   members = [segment_user.id-razwjqyaqEwjZUqcMzxMb6.email]
-  name    = "extensions-demo-1 - Bug bash create empty groups"
-  permissions = []
+  name    = "extensions-demo-1 - Bug bash create duped group (owner)"
+  permissions = [
+    {
+      resources = [
+        {
+          id = "9uXW1jxiwcBVdYmovz4gki"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1WDUuRLxv84rrfCNUwvkrRtkxnS"
+    },
+  ]
 }
