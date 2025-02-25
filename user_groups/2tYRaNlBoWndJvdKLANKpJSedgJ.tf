19cf064a82c0a09a92c3ec595719466dd5ddd60c
Segment Git Sync <friends@segment.com>
2025-02-25 23:44:44 +0000

    User Group '2tYRaNlBoWndJvdKLANKpJSedgJ' created by jair.aviles@segment.com

---
+++ user_groups/2tYRaNlBoWndJvdKLANKpJSedgJ.tf
@@ -0,0 +1,22 @@
+import {
+  to = segment_user_group.id-2tYRaNlBoWndJvdKLANKpJSedgJ
+  id = "2tYRaNlBoWndJvdKLANKpJSedgJ"
+}
+
+resource "segment_user_group" "id-2tYRaNlBoWndJvdKLANKpJSedgJ" {
+  members = []
+  name    = "APICS-3020"
+  permissions = [
+    {
+      resources = [
+        {
+          id = "9uXW1jxiwcBVdYmovz4gki"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1WDUuRLxv84rrfCNUwvkrRtkxnS"
+    },
+  ]
+}
