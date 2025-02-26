8dcd255005396163ab11540c6a1d96019373466d
Segment Git Sync <friends@segment.com>
2025-02-26 19:18:43 +0000

    User Group '2tNeVx76Veuj6aJj8TRxgVuD7AQ' updated by Public API token 'Autogenerated SSO Token'

--- user_groups/2tNeVx76Veuj6aJj8TRxgVuD7AQ.tf
+++ user_groups/2tNeVx76Veuj6aJj8TRxgVuD7AQ.tf
@@ -5,7 +5,7 @@ import {
 
 resource "segment_user_group" "id-2tNeVx76Veuj6aJj8TRxgVuD7AQ" {
   members = ["jair.aviles@segment.com"]
-  name    = "extensions-demo-1 - Workspace Owner"
+  name    = "Segment : extensions-demo-1 - Workspace Owner"
   permissions = [
     {
       resources = [
