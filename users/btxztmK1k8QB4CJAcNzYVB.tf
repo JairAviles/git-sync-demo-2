f2961b2e718b4b768bb9f3b89a371b374ad68e67
Segment Git Sync <friends@segment.com>
2025-02-26 00:33:09 +0000

    User 'btxztmK1k8QB4CJAcNzYVB' added by jeusebio@twilio.com

---
+++ users/btxztmK1k8QB4CJAcNzYVB.tf
@@ -0,0 +1,21 @@
+import {
+  to = segment_user.id-btxztmK1k8QB4CJAcNzYVB
+  id = "btxztmK1k8QB4CJAcNzYVB"
+}
+
+resource "segment_user" "id-btxztmK1k8QB4CJAcNzYVB" {
+  email = "jeusebio@twilio.com"
+  permissions = [
+    {
+      resources = [
+        {
+          id = "9uXW1jxiwcBVdYmovz4gki"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1WDUuRLxv84rrfCNUwvkrRtkxnS"
+    },
+  ]
+}
