7cfe274f1f0f2805e70ff47e212969d3e4a479bb
Segment Git Sync <friends@segment.com>
2025-02-26 19:31:37 +0000

    User Group '2talvWZVkU1yog0hpqWNYml2kzx' created by jeusebio@twilio.com

---
+++ user_groups/2talvWZVkU1yog0hpqWNYml2kzx.tf
@@ -0,0 +1,22 @@
+import {
+  to = segment_user_group.id-2talvWZVkU1yog0hpqWNYml2kzx
+  id = "2talvWZVkU1yog0hpqWNYml2kzx"
+}
+
+resource "segment_user_group" "id-2talvWZVkU1yog0hpqWNYml2kzx" {
+  members = []
+  name    = "extensions-demo-1 - Workspace Owner"
+  permissions = [
+    {
+      resources = [
+        {
+          id = "9uXW1jxiwcBVdYmovz4gki"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1WDUuRLxv84rrfCNUwvkrRtkxnS"
+    },
+  ]
+}
