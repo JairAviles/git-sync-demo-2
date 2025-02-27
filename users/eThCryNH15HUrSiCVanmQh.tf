ff050415532bbc79cd3d54267bd373a81a5aafba
Segment Git Sync <friends@segment.com>
2025-02-27 19:30:53 +0000

    User 'eThCryNH15HUrSiCVanmQh' added by jeusebio+uas-test@twilio.com

---
+++ users/eThCryNH15HUrSiCVanmQh.tf
@@ -0,0 +1,21 @@
+import {
+  to = segment_user.id-eThCryNH15HUrSiCVanmQh
+  id = "eThCryNH15HUrSiCVanmQh"
+}
+
+resource "segment_user" "id-eThCryNH15HUrSiCVanmQh" {
+  email = "jeusebio+uas-test@twilio.com"
+  permissions = [
+    {
+      resources = [
+        {
+          id = "9uXW1jxiwcBVdYmovz4gki"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1aEWAcU0fArxScocwH9xxLgW7r3"
+    },
+  ]
+}
