951b9d83a5965be961cf4d3d9d76267963fb1322
Segment Git Sync <friends@segment.com>
2025-06-05 23:13:02 +0000

    Label 'environment:label01' created by jair.aviles@segment.com

---
+++ labels/environment_label01.tf
@@ -0,0 +1,10 @@
+import {
+  to = segment_label.id-environment_label01
+  id = "environment:label01"
+}
+
+resource "segment_label" "id-environment_label01" {
+  description = null
+  key         = "environment"
+  value       = "label01"
+}
