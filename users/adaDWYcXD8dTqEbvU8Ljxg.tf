d9dc86cf3a583bc886b8e4ea8032af654c88a516
Segment Git Sync <friends@segment.com>
2025-08-01 21:15:46 +0000

    User 'adaDWYcXD8dTqEbvU8Ljxg' added by jair.aviles+apics-1907-non-org@segment.com

---
+++ users/adaDWYcXD8dTqEbvU8Ljxg.tf
@@ -0,0 +1,21 @@
+import {
+  to = segment_user.id-adaDWYcXD8dTqEbvU8Ljxg
+  id = "adaDWYcXD8dTqEbvU8Ljxg"
+}
+
+resource "segment_user" "id-adaDWYcXD8dTqEbvU8Ljxg" {
+  email = "jair.aviles+apics-1907-non-org@segment.com"
+  permissions = [
+    {
+      resources = [
+        {
+          id = "9uXW1jxiwcBVdYmovz4gki"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1WDUuRLxv84rrfCNUwvkrRtkxnS"
+    },
+  ]
+}
