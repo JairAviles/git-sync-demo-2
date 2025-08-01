1e8cdb1b0a0eec52543ec3655630bf0dd96ec6e1
Segment Git Sync <friends@segment.com>
2025-08-01 18:21:54 +0000

    User 'Gy1L9xmSNSfZiTw2Ftb4w' added by jair.aviles+apics-1907@segment.com

---
+++ users/Gy1L9xmSNSfZiTw2Ftb4w.tf
@@ -0,0 +1,21 @@
+import {
+  to = segment_user.id-Gy1L9xmSNSfZiTw2Ftb4w
+  id = "Gy1L9xmSNSfZiTw2Ftb4w"
+}
+
+resource "segment_user" "id-Gy1L9xmSNSfZiTw2Ftb4w" {
+  email = "jair.aviles+apics-1907@segment.com"
+  permissions = [
+    {
+      resources = [
+        {
+          id = "9uXW1jxiwcBVdYmovz4gki"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1WDUuRLxv84rrfCNUwvkrRtkxnS"
+    },
+  ]
+}
