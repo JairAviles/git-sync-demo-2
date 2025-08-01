92db88241c62d619f38280c004dd966ec32cf2af
Segment Git Sync <friends@segment.com>
2025-08-01 19:30:49 +0000

    User 'cfvtarg7sU3AfMZWrbX5mL' added by jair.aviles+apics-1907-non-org@segment.com

---
+++ users/cfvtarg7sU3AfMZWrbX5mL.tf
@@ -0,0 +1,21 @@
+import {
+  to = segment_user.id-cfvtarg7sU3AfMZWrbX5mL
+  id = "cfvtarg7sU3AfMZWrbX5mL"
+}
+
+resource "segment_user" "id-cfvtarg7sU3AfMZWrbX5mL" {
+  email = "jair.aviles+apics-1907-non-org@segment.com"
+  permissions = [
+    {
+      resources = [
+        {
+          id = "9uXW1jxiwcBVdYmovz4gki"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1WDUuRLxv84rrfCNUwvkrRtkxnS"
+    },
+  ]
+}
