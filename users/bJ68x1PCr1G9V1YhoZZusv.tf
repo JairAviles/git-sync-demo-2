966f08dc70769ffc2368346df8e2e023d18ffee5
Segment Git Sync <friends@segment.com>
2025-08-27 23:07:50 +0000

    User 'bJ68x1PCr1G9V1YhoZZusv' added by jair.aviles+apics-3520@segment.com

---
+++ users/bJ68x1PCr1G9V1YhoZZusv.tf
@@ -0,0 +1,21 @@
+import {
+  to = segment_user.id-bJ68x1PCr1G9V1YhoZZusv
+  id = "bJ68x1PCr1G9V1YhoZZusv"
+}
+
+resource "segment_user" "id-bJ68x1PCr1G9V1YhoZZusv" {
+  email = "jair.aviles+apics-3520@segment.com"
+  permissions = [
+    {
+      resources = [
+        {
+          id = "j7RrR1hLgqQhqYYV9rNRB1"
+          labels = [
+          ]
+          type = "SOURCE"
+        },
+      ]
+      role_id = "1WDUuSF1kN1xis2lp09RVKOsnnL"
+    },
+  ]
+}
