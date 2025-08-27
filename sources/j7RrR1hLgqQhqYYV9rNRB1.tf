d615c736783e6b55d6b25a1bdcf38d1f0f1de521
Segment Git Sync <friends@segment.com>
2025-08-27 22:41:45 +0000

    Source 'j7RrR1hLgqQhqYYV9rNRB1' created by jair.aviles@segment.com

---
+++ sources/j7RrR1hLgqQhqYYV9rNRB1.tf
@@ -0,0 +1,18 @@
+import {
+  to = segment_source.id-j7RrR1hLgqQhqYYV9rNRB1
+  id = "j7RrR1hLgqQhqYYV9rNRB1"
+}
+
+resource "segment_source" "id-j7RrR1hLgqQhqYYV9rNRB1" {
+  enabled = true
+  labels  = null
+  metadata = {
+    id = "IqDTy1TpoU"
+  }
+  name = "SECOPS-17654 "
+  settings = jsonencode({
+    apiHost     = "api.segment.build/v1"
+    website_url = "https://example.com"
+  })
+  slug = "secops_17654"
+}
