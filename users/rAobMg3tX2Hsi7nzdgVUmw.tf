9eff96edc75ad35daa1ff0cacf121500e736e111
Segment Git Sync <friends@segment.com>
2025-10-30 21:22:59 +0000

    User 'rAobMg3tX2Hsi7nzdgVUmw' added by jair.aviles+test@segment.com

---
+++ users/rAobMg3tX2Hsi7nzdgVUmw.tf
@@ -0,0 +1,21 @@
+import {
+  to = segment_user.id-rAobMg3tX2Hsi7nzdgVUmw
+  id = "rAobMg3tX2Hsi7nzdgVUmw"
+}
+
+resource "segment_user" "id-rAobMg3tX2Hsi7nzdgVUmw" {
+  email = "jair.aviles+test@segment.com"
+  permissions = [
+    {
+      resources = [
+        {
+          id = "9uXW1jxiwcBVdYmovz4gki"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1WDUuRLxv84rrfCNUwvkrRtkxnS"
+    },
+  ]
+}
