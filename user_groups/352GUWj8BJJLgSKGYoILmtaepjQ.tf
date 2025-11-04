ee1204e48ba2f089ef68d0f58b03d7c1b10fef1e
Segment Git Sync <friends@segment.com>
2025-11-04 23:59:39 +0000

    User Group '352GUWj8BJJLgSKGYoILmtaepjQ' created by jair.aviles@segment.com

---
+++ user_groups/352GUWj8BJJLgSKGYoILmtaepjQ.tf
@@ -0,0 +1,22 @@
+import {
+  to = segment_user_group.id-352GUWj8BJJLgSKGYoILmtaepjQ
+  id = "352GUWj8BJJLgSKGYoILmtaepjQ"
+}
+
+resource "segment_user_group" "id-352GUWj8BJJLgSKGYoILmtaepjQ" {
+  members = []
+  name    = "extensions-demo-1 - Workspace Owner"
+  permissions = [
+    {
+      resources = [
+        {
+          id = "9uXW1jxiwcBVdYmovz4gki"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1WDUuRLxv84rrfCNUwvkrRtkxnS"
+    },
+  ]
+}
