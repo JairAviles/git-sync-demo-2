030c6702c161bf5d19b04804fb7fa08ddbbd3324
Segment Git Sync <friends@segment.com>
2025-11-05 00:36:09 +0000

    User Group '352GnEuSPmmMMqkCJ5YnKQKIWdj' updated by Public API token 'Autogenerated SSO Token'

--- user_groups/352GnEuSPmmMMqkCJ5YnKQKIWdj.tf
+++ user_groups/352GnEuSPmmMMqkCJ5YnKQKIWdj.tf
@@ -4,7 +4,7 @@ import {
 }
 
 resource "segment_user_group" "id-352GnEuSPmmMMqkCJ5YnKQKIWdj" {
-  members = []
+  members = ["jeusebio@twilio.com"]
   name    = "extensions-demo-1 - Functions Admin"
   permissions = [
     {
