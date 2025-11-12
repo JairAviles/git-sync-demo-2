b1e49d95c81252ef55acdbcf000dd52f8c5b8771
Segment Git Sync <friends@segment.com>
2025-11-12 17:26:48 +0000

    Label 'demo:test' created by jair.aviles@segment.com

---
+++ labels/demo_test.tf
@@ -0,0 +1,10 @@
+import {
+  to = segment_label.id-demo_test
+  id = "demo:test"
+}
+
+resource "segment_label" "id-demo_test" {
+  description = null
+  key         = "demo"
+  value       = "test"
+}
