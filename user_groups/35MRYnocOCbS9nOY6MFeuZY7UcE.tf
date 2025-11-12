4a303232ba71bd3085d1c455a434ed988c15103a
Segment Git Sync <friends@segment.com>
2025-11-12 03:26:44 +0000

    User Group '35MRYnocOCbS9nOY6MFeuZY7UcE' created by jair.aviles@segment.com

---
+++ user_groups/35MRYnocOCbS9nOY6MFeuZY7UcE.tf
@@ -0,0 +1,33 @@
+import {
+  to = segment_user_group.id-35MRYnocOCbS9nOY6MFeuZY7UcE
+  id = "35MRYnocOCbS9nOY6MFeuZY7UcE"
+}
+
+resource "segment_user_group" "id-35MRYnocOCbS9nOY6MFeuZY7UcE" {
+  members = []
+  name    = "extensions-demo-1 - Source and Warehouse Admin"
+  permissions = [
+    {
+      resources = [
+        {
+          id = "9uXW1jxiwcBVdYmovz4gki"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1WDUuSF1kN1xis2lp09RVKOsnnL"
+    },
+    {
+      resources = [
+        {
+          id = "9uXW1jxiwcBVdYmovz4gki"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1WDUuY68jLTuIRC6DzqM6zKnkSf"
+    },
+  ]
+}
