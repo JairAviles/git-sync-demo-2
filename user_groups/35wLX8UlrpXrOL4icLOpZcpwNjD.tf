620b2dbaf9ac42a38b97d875941c91f64b32f8d6
Segment Git Sync <friends@segment.com>
2025-11-24 20:30:25 +0000

    User Group '35wLX8UlrpXrOL4icLOpZcpwNjD' created by jair.aviles@segment.com

---
+++ user_groups/35wLX8UlrpXrOL4icLOpZcpwNjD.tf
@@ -0,0 +1,33 @@
+import {
+  to = segment_user_group.id-35wLX8UlrpXrOL4icLOpZcpwNjD
+  id = "35wLX8UlrpXrOL4icLOpZcpwNjD"
+}
+
+resource "segment_user_group" "id-35wLX8UlrpXrOL4icLOpZcpwNjD" {
+  members = []
+  name    = "extensions-demo - Observability Admin PII"
+  permissions = [
+    {
+      resources = [
+        {
+          id = "9uXW1jxiwcBVdYmovz4gki"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1ZxBLEwfZgYG3olVPxkc7e35Wue"
+    },
+    {
+      resources = [
+        {
+          id = "9uXW1jxiwcBVdYmovz4gki"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "Ez2WdbOWYfaD4aBhNghPaLjbHdf"
+    },
+  ]
+}
