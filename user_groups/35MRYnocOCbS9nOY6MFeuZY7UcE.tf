9e165561258e0e98dd434a2418ded2ab77e32b00
Segment Git Sync <friends@segment.com>
2025-12-01 22:33:09 +0000

    User Group '35MRYnocOCbS9nOY6MFeuZY7UcE' updated by jair.aviles@segment.com

--- user_groups/35MRYnocOCbS9nOY6MFeuZY7UcE.tf
+++ user_groups/35MRYnocOCbS9nOY6MFeuZY7UcE.tf
@@ -5,7 +5,7 @@ import {
 
 resource "segment_user_group" "id-35MRYnocOCbS9nOY6MFeuZY7UcE" {
   members = []
-  name    = "extensions-demo-1 - Source and Warehouse Admin"
+  name    = "extensions-demo-1 - Source Admin"
   permissions = [
     {
       resources = [
@@ -17,17 +17,6 @@ resource "segment_user_group" "id-35MRYnocOCbS9nOY6MFeuZY7UcE" {
         },
       ]
       role_id = "1WDUuSF1kN1xis2lp09RVKOsnnL"
-    },
-    {
-      resources = [
-        {
-          id = "9uXW1jxiwcBVdYmovz4gki"
-          labels = [
-          ]
-          type = "WORKSPACE"
-        },
-      ]
-      role_id = "1WDUuY68jLTuIRC6DzqM6zKnkSf"
     },
   ]
 }
