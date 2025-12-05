b8f10f950590f2db20835a6777174963863b0832
Segment Git Sync <friends@segment.com>
2025-12-05 01:52:08 +0000

    Source '3Jja1PAsonX8mAk46FkdLp' created by jeusebio@twilio.com

---
+++ sources/3Jja1PAsonX8mAk46FkdLp.tf
@@ -0,0 +1,15 @@
+import {
+  to = segment_source.id-3Jja1PAsonX8mAk46FkdLp
+  id = "3Jja1PAsonX8mAk46FkdLp"
+}
+
+resource "segment_source" "id-3Jja1PAsonX8mAk46FkdLp" {
+  enabled = true
+  labels  = null
+  metadata = {
+    id = "iUM16Md8P2"
+  }
+  name     = "APICS-3861 - with out labels"
+  settings = jsonencode({})
+  slug     = "apics_3861_with_out_labels"
+}
