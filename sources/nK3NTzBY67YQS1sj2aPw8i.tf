5e55546d3fdf21527438dcc6f9b85312bd0a0cf8
Segment Git Sync <friends@segment.com>
2025-12-05 23:21:32 +0000

    Source 'nK3NTzBY67YQS1sj2aPw8i' created by jeusebio@twilio.com

---
+++ sources/nK3NTzBY67YQS1sj2aPw8i.tf
@@ -0,0 +1,15 @@
+import {
+  to = segment_source.id-nK3NTzBY67YQS1sj2aPw8i
+  id = "nK3NTzBY67YQS1sj2aPw8i"
+}
+
+resource "segment_source" "id-nK3NTzBY67YQS1sj2aPw8i" {
+  enabled = true
+  labels  = null
+  metadata = {
+    id = "iUM16Md8P2"
+  }
+  name     = "APICS-3861 - with no labels"
+  settings = jsonencode({})
+  slug     = "apics_3861_with_no_labels"
+}
