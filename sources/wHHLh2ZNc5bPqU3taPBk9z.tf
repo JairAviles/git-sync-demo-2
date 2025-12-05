a778e60a7261479541ad3a9491d4fd74db34160f
Segment Git Sync <friends@segment.com>
2025-12-05 01:51:20 +0000

    Source 'wHHLh2ZNc5bPqU3taPBk9z' created by jeusebio@twilio.com

---
+++ sources/wHHLh2ZNc5bPqU3taPBk9z.tf
@@ -0,0 +1,15 @@
+import {
+  to = segment_source.id-wHHLh2ZNc5bPqU3taPBk9z
+  id = "wHHLh2ZNc5bPqU3taPBk9z"
+}
+
+resource "segment_source" "id-wHHLh2ZNc5bPqU3taPBk9z" {
+  enabled = true
+  labels  = null
+  metadata = {
+    id = "iUM16Md8P2"
+  }
+  name     = "APICS-3861 - with out labels"
+  settings = jsonencode({})
+  slug     = "apics_3861_with_out_labels"
+}
