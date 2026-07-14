6b2062928fcf1d728d68807a7fbf5f29825983fc
Segment Git Sync <friends@segment.com>
2026-07-14 20:30:52 +0000

    Full sync

---
+++ user_groups/36OPQ4RfUhY4Kr2oR7zYx3rnJOi.tf
@@ -0,0 +1,22 @@
+import {
+  to = segment_user_group.id-36OPQ4RfUhY4Kr2oR7zYx3rnJOi
+  id = "36OPQ4RfUhY4Kr2oR7zYx3rnJOi"
+}
+
+resource "segment_user_group" "id-36OPQ4RfUhY4Kr2oR7zYx3rnJOi" {
+  members = []
+  name    = "APICS-3861"
+  permissions = [
+    {
+      resources = [
+        {
+          id = "9uXW1jxiwcBVdYmovz4gki"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1WDUuSF1kN1xis2lp09RVKOsnnL"
+    },
+  ]
+}
