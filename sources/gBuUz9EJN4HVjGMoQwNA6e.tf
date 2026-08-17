f3c4e9c30ea965685f637873662ae01e629f27a2
Segment Git Sync <friends@segment.com>
2026-08-17 16:36:59 +0000

    Full sync

---
+++ sources/gBuUz9EJN4HVjGMoQwNA6e.tf
@@ -0,0 +1,15 @@
+import {
+  to = segment_source.id-gBuUz9EJN4HVjGMoQwNA6e
+  id = "gBuUz9EJN4HVjGMoQwNA6e"
+}
+
+resource "segment_source" "id-gBuUz9EJN4HVjGMoQwNA6e" {
+  enabled = true
+  labels  = null
+  metadata = {
+    id = "dBg7wLNWBb"
+  }
+  name     = "Personas Test space"
+  settings = jsonencode({})
+  slug     = "personas_test-space"
+}
