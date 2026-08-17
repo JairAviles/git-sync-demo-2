f241e29b478a5e7d2d19948f98a5c2d9a240b13e
Segment Git Sync <friends@segment.com>
2026-08-17 16:37:02 +0000

    Full sync

---
+++ users/razwjqyaqEwjZUqcMzxMb6.tf
@@ -0,0 +1,21 @@
+import {
+  to = segment_user.id-razwjqyaqEwjZUqcMzxMb6
+  id = "razwjqyaqEwjZUqcMzxMb6"
+}
+
+resource "segment_user" "id-razwjqyaqEwjZUqcMzxMb6" {
+  email = "jair.aviles@segment.com"
+  permissions = [
+    {
+      resources = [
+        {
+          id = "9uXW1jxiwcBVdYmovz4gki"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1WDUuRLxv84rrfCNUwvkrRtkxnS"
+    },
+  ]
+}
